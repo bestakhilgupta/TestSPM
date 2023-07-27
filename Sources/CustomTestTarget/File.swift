8094ad8fa828e39415ac15a8568c3540600230fd
bestakhilgupta <50282905+bestakhilgupta@users.noreply.github.com>
2023-07-27 18:38:23 -0500

    initial commit

---
+++ Sources/CustomTestTarget/File.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by Akhil Gupta on 7/27/23.
+//
+
+import Foundation
+public struct TestLibrary {
+    public init() {}
+    
+    public var testString = "Hi"
+}
